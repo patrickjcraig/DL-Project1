9fd449616847bb78b3214a4bc26037868f2dda75
patrickjcraig <pcraig1@ufl.edu>
2024-10-29 19:55:03 -0700

    Push changes for MLP and MLP testing for better function imports and for the input layer stuff

--- models/MLP.py
+++ models/MLP.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Input, Flatten
 from tensorflow.keras.optimizers import Adam, SGD, RMSprop
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.initializers import GlorotUniform
@@ -10,6 +10,8 @@ def create_mlp(input_shape, num_classes, layer_units=None, activation='relu', le
         layer_units = {i: 128 for i in range(2)}  # Default to 2 hidden layers with 128 units each
     
     model = Sequential()
+    model.add(Input(shape=(input_shape)))
+    model.add(Flatten())
     model.add(Dense(layer_units.get(0, 128), activation=activation, input_shape=(input_shape,), kernel_initializer=GlorotUniform(seed=42)))
     model.add(Dropout(dropout_rate))
     for i in range(1, len(layer_units)):
@@ -33,7 +35,10 @@ def create_mlp(input_shape, num_classes, layer_units=None, activation='relu', le
     
     return model
 
-# Example usage:
-# layer_units = {0: 64, 1: 128, 2: 256}
-# model = create_mlp(input_shape=784, num_classes=10, layer_units=layer_units)
-# model.summary()
+    if __name__ == "__main__":
+        input_shape = (28,28,1)  # Example for flattened 28x28 images
+        num_classes = 10   # Example for 10 classes (e.g., digits 0-9)
+        layer_units = {0: 256, 1: 128}  # Example layer configuration
+
+        model = create_mlp(input_shape, num_classes, layer_units, activation='relu', learning_rate=0.001, optimizer='adam', dropout_rate=0.5)
+        model.summary()
